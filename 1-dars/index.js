be1291937e51dc85744cc818e165e5898b3a654a
gaybullayev-303 <hanifaqosimova701@gmail.com>
2025-12-19 17:39:19 +0500

    2-masala

--- 1-dars/index.js
+++ 1-dars/index.js
@@ -1,2 +1,6 @@
 // let maps = (x) => x.map(n => n * 2)
 // console.log(maps([1,2,3]));
+
+
+
+
---
+++ 2-dars/index.js
@@ -0,0 +1,15 @@
+// let x = [9, 3, '7', '3'];
+
+
+
+
+
+// function sumMix(x){
+//   let sum = 0;
+//     x.array.forEach((x) => ())
+//       if(x += number(x) + sum ){
+        
+//        console.log(sum);
+       
+//     }
+//   }
