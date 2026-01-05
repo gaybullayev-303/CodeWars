301f006eb59e8ae8b820c3bde912c87b5df04478
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-05 16:59:06 +0500

    7-masala

---
+++ 7-masala/index.js
@@ -0,0 +1,9 @@
+// Find the area of a rectangle when provided with one diagonal and 
+// one side of the rectangle. If the input diagonal is less than or equal
+//  to the length of the side, return "Not a rectangle".
+//  If the resultant area has decimals round it to two places.
+
+// --------------------------------------------------
+// ISHLANISHI -->
+
+// let area = (d,l)=> d<=l ? "not a rectangle" : +(b = Math.sqrt(d**2 - l**2)*l).toFixed(2);
