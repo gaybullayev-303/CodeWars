21ee780b725ac7218259419bd9b34e3d3378809a
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-06 19:45:43 +0500

    8-masala

---
+++ 8-masala/index.js
@@ -0,0 +1,39 @@
+// // let arr = [a,b,c,d]
+// let arithmetic = (a, b, operator)=> {
+//   arr.a = arithmetic.a + arithmetic.b;
+//   arr.b = arithmetic.a - arithmetic.b;
+//   arr.c = arithmetic.a * arithmetic.b;
+//   arr.d = arithmetic.a / arithmetic.b;
+// }
+// arithmetic(5,2)
+// console.log(arr)
+
+
+// ======================================
+
+// function arithmetic(a, b, operator){
+//   return a+b, a-b,a*b,a/b
+// }
+
+
+// ===========================================
+// function arithmetic(a, b, operator){
+//   let obj = {
+//     add:a+b,
+//     subtract:a-b,
+//     multiply:a*b,
+//     divide:a/b
+    
+//   }
+//   return obj[operator]
+// }
+
+
+
+
+// -----------------------------------------
+
+// // 3 ---
+//   function solution(fullText, search) {
+//   return fullText.split(search).length -1
+// }
