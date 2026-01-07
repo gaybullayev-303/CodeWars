0a34fe77e82dc671b06903655df10463b628fab5
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-07 17:39:57 +0500

    9-masala

---
+++ 9-masala/index.js
@@ -0,0 +1,17 @@
+// let array = [4, 8,6]
+
+// function consecutive(array) {
+//   let min = Math.min(...array)
+//   let max = Math.max(...array)
+//   let newarr = [];
+//   for(i = min ; i <= max; i++)newarr.push(i)
+//   return (newarr.length - array.length);
+// }
+// console.log();
+
+
+// ----------------------------------------------------------
+
+// var OrderPeople = function(people){
+//   return people.sort((a,b) => a.age - b.age ); 
+// }
