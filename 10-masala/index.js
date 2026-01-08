fbb0abea61c0c0a10540c6570bcdd3c9a5bd03b1
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-08 20:52:39 +0500

    10-masala

---
+++ 10-masala/index.js
@@ -0,0 +1,15 @@
+// let a = ['a', 'b', 'c'];
+// let b = [1, 2, 3]
+  
+
+// function exchangeWith(a, b) {
+//   let acopy = [...a].reverse();
+//   let bcopy = [...b].reverse();
+//   a.length = 0
+//   b.length = 0
+//   a.push(...bcopy)
+//   b.push(...acopy)
+// }
+
+// -------------------------------------------------------------------
+
