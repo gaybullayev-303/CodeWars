e2572e559e4137ff034f70c415c5700f8ee61298
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-10 18:26:20 +0500

    11-masala

---
+++ 11-masala/index.js
@@ -0,0 +1,8 @@
+// function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//   return laLigaGoals+copaDelReyGoals+championsLeagueGoals
+// }
+// ===================================================================
+
+// 2---
+// let chromosomeCheck = (sperm)=> sperm.includes("Y") ? "Congratulations! You're going to have a son."
+//  : "Congratulations! You're going to have a daughter."
