b1b4225552699273573f2def945f4445d095ed97
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-11 21:02:06 +0500

    12-masala

---
+++ 12-masala/index.js
@@ -0,0 +1,6 @@
+// function moveZeros(arr){
+//   let a = arr.filter(x => x !== 0)
+//   let b = arr.filter(x => x === 0)
+//     return a.concat(b)
+// }
+// --------------------------------------------------------
