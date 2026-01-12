a9928010056591d53edf1696ec168692c1e38409
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-12 21:54:46 +0500

    13-masala

---
+++ 13-masala/index.js
@@ -0,0 +1,9 @@
+// function doubleCheck(str){
+//     str = str.toLowerCase()
+//   for (let i = 0 ; i < str.length - 1 ; i++){
+//     if(str[i] === str[i + 1]){
+//       return true
+//     }
+//   }
+//   return false
+// }
