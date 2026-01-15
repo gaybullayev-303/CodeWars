f8478cf823dc6fd6b3a84a5e1864b0d5ca590824
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-15 21:38:00 +0500

    5-masala

---
+++ 5-masala/index.js
@@ -0,0 +1,6 @@
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+// let arr = ['hello', 'world', 'this', 'is', 'great'];
+// arr = arr.toString();
+// let newarr = arr.map(item => item.replace(","))
+// console.log(newarr);
