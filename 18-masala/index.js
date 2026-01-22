9865e66d354957fae669430c3a0045270f836cdc
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-22 20:41:37 +0500

    18-masala

---
+++ 18-masala/index.js
@@ -0,0 +1,8 @@
+// let add = (n1, n2, max = 0, str = "") => {
+//   n1 > n2 ? (max = n1) : (max = n2);
+//   let arr1 = `${n1}`.split("");
+//   let arr2 = `${n2}`.split("");
+//   for (let i = 1; i <= `${max}`.length; i++)
+//     str = (+arr1.at(-i) || 0) + (+arr2.at(-i) || 0) + str;
+//   return +str;
+// };
