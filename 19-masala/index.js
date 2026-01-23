4a5d5df11537074b505733f4bb3221594506bdfb
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-23 18:02:44 +0500

    19-masala

---
+++ 19-masala/index.js
@@ -0,0 +1,9 @@
+// function type(v) {
+//   if(typeof v === 'object'){
+//     if(Array.isArray(v)) return 'array'
+//     if(v == null) return 'null'
+//     if(v == undefined) return 'undefined'
+//     if(isFinite(v) === true) return 'date'
+//     return 'object'
+//   }
+// }
