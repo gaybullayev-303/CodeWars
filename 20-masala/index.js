cfbe38ef94f111240cf1cf6bb1171c316d4005fc
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-24 17:43:23 +0500

    20-masala

---
+++ 20-masala/index.js
@@ -0,0 +1,9 @@
+// function findMissingNumbers(arr){
+//   let sum = [];
+//   for(let i = arr[0];i <= arr[arr.length -1]; i++){
+//     if(!arr.includes(i)){
+//        sum.push(i)
+//       }
+//   }
+//    return sum
+// }
