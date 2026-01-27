ae79bf388d7b10846da2aaabb51a17830a48131f
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-01-27 18:00:12 +0500

    21-masala

---
+++ 21-masala/index.js
@@ -0,0 +1,13 @@
+// function createPhoneNumber(numbers){
+//   let overal = "";
+//   let part1 = 0;
+//   let part2 = 0;
+//   let part3 = 0;
+//   if(numbers.length == 10){
+//    part1 = numbers.slice(0 , 3)
+//        part2 = numbers.slice(3 , 6)
+//        part3 = numbers.slice(6)
+//         overal = `(${part1}) ${part2}-${part3}`
+//         return overal.replaceAll(",", "")
+//     }                       
+// }
