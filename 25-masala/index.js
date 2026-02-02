7ec04cde2ea63cc2f9524900d731ee863cb0d24b
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-02-02 22:13:48 +0500

    25-masala

---
+++ 25-masala/index.js
@@ -0,0 +1,8 @@
+// function howMuchCoffee(events) {
+// let coffe = 0;
+//   for(let e of events){
+//     if(["dog" , "cat" , "movie" , "cw"].includes(e))   coffe =+1
+//      else if(["DOG" , "CAT" , "MOVIE" , "CW"].includes(e))  coffe =+2
+//   }
+//   return coffe > 3 ? "You need extra sleep" : coffe
+//   }
