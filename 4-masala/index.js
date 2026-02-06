af448475c930073bf116d175bc18f14137b0b3d2
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-02-07 00:40:36 +0500

    26-masala

---
+++ 4-masala/index.js
@@ -0,0 +1,14 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+// let sum1 = 0;
+// let sum2 = 0;
+// let arr2 = []
+// const f = (arr.filter((obj) => ))
+// arr = arr.filter((obj) => )
+    // let arr2 = arr.sort((a,b) => a,b);
+    // console.log(arr);
+    // arr.forEach((obj) => {
+    //     arr.filter((obj) => arr.obj === 1 ? + sum2 : + sum1 )
+    // })
+    // arr2 = sum1 + sum2;
+    // console.log(obj);
+    
