b183ec419b6faa8e76f87d6feb829f3986d3a32b
gaybullayev-303 <hanifaqosimova701@gmail.com>
2026-02-09 19:02:35 +0500

    28-masala

--- 3-masala/index.js
+++ 3-masala/index.js
@@ -1,15 +1,10 @@
-// let x = [1,2,3,4] 
-
-//  [1 * 2 * 3 * 4] = 24;  bo'lishi kerak
-
-// function grow(x){
-//   let sum = 1;
-//   x.forEach((value) => {
-//   sum = sum * value;
-   
-// })
-//   return sum;
+// let x = [1, 2, 3, 4]
+// //  [1 * 2 * 3 * 4] = 24;  bo'lishi kerak
+// function grow(x) {
+//     let sum = 1;
+//     x.forEach((value) => {
+//         sum = sum * value;
+//     })
+//     return sum;
 // }
-
-// console.log(sum);
-
+// console.log(grow(x));
